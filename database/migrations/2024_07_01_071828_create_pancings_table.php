f4b20cbc5a3d49d6834dfab2dda19a11c461c241
saktiworkstation <saktiworkstation@gmail.com>
2024-07-02 20:49:11 +0700

    merancang skema pancing

--- database/migrations/2024_07_01_071828_create_pancings_table.php
+++ database/migrations/2024_07_01_071828_create_pancings_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('pancings', function (Blueprint $table) {
             $table->id();
+            $table->string('nama', 100);
+            $table->string('alamat', 255)->nullable();
+            $table->string('no_telepon', 15)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->date('tanggal_daftar');
             $table->timestamps();
         });
     }
